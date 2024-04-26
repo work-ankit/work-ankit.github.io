fbb424a4814e0b78dc4079b72671ae5b56fc68c8
work-ankit <160381855+work-ankit@users.noreply.github.com>
2024-04-27 02:19:26 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,9 +34,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "fbbe13134281d44f28fca44f08a72188
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e831c9f24b68ea24d37b1d2a09167742",
-"/": "e831c9f24b68ea24d37b1d2a09167742",
-"main.dart.js": "42a897646de340eca7ad93e5a04d378d",
+"index.html": "c56b910709062f56ee61b872b42b4e61",
+"/": "c56b910709062f56ee61b872b42b4e61",
+"main.dart.js": "393c1795c53e4f12c36bb391e513d3f4",
 "manifest.json": "e792d02060e65dfeb4b27c3df0e4d481",
 "version.json": "6d488a8c032e114749c60ec6a1643d03"};
 // The application shell files that are downloaded before a service worker can
